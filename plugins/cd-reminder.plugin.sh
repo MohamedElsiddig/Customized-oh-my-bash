3cb29c7ad5321fcd7c1bd05750610477a5b5f73c
MohamedElsiddig <mxlr7siddig@gmail.com>
2019-11-24 10:51:56 +0200

    cd-reminder plugin

---
+++ plugins/cd-reminder.plugin.sh
@@ -0,0 +1,41 @@
+cite about-plugin
+about-plugin 'Plugin to display reminders when cd-ing into specified directories'
+
+# calls the builtin cd command, and then optionally cat's the .cd-reminder file
+# in the directory if it exists
+reminder_cd() {
+if [[ `declare -F __enhancd::cd` ]]
+	then
+    		__enhancd::cd "$@" && { [[ ! -a .cd-reminder ]] || cat .cd-reminder 1>&2; }
+	else
+		cd "$@" && { [[ ! -a .cd-reminder ]] || cat .cd-reminder 1>&2; }
+	fi
+}
+
+# Either creates an empty .cd-reminder file, or if an argument is included
+# then creates and appends to that file the first argument
+new_reminder() {
+    if [[ $# -eq 0 ]]; then
+        touch .cd-reminder
+    fi
+
+    if [[ $# -gt 0 ]]; then
+        echo $@ >> .cd-reminder
+    fi
+}
+
+alias cd=reminder_cd
+
+_insert_git_exclude() {
+  echo ".cd-reminder" >> $(git config --global core.excludesfile)
+}
+
+git_exclude_cd_reminder() {
+  if [[ -a $(git config --global core.excludesfile) ]]; then
+    _insert_git_exclude
+  else
+    touch "$HOME/.gitignore-global"
+    git config --global core.excludesfile "$HOME/.gitignore-global"
+    _insert_git_exclude
+  fi
+}
