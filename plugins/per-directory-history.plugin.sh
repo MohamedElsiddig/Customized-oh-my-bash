3bafe3c6417c13c5e04c4218794b4f45e60ca5a6
MohamedElsiddig <mxlr7siddig@gmail.com>
2019-11-27 19:34:31 +0200

    per directory history implementation

---
+++ plugins/per-directory-history.plugin.sh
@@ -0,0 +1,33 @@
+cite about-plugin
+about-plugin 'An implementation of per directory history for bash'
+
+function per-dir_bash_history()
+{
+    #if this directory is writable then write to directory-based history file
+    #otherwise write history in the usual home-based history file                    
+    tmpDir=$PWD
+    #echo "#"`date '+%s'` >> $HISTFILE
+    #echo $USER' has exited '$PWD' for '$@ >> $HISTFILE
+    cd "$@" # do actual cd
+    if [ -w $PWD ]; then export HISTFILE="$PWD/.dir_bash_history"; touch $HISTFILE; chmod --silent 777 $HISTFILE;
+    else export HISTFILE="$HOME/.bash_history";
+    fi
+    #echo "#"`date '+%s'` >> $HISTFILE
+    #echo $USER' has entered '$PWD' from '$OLDPWD >> $HISTFILE
+
+}
+alias cd="per-dir_bash_history"
+#initial shell opened                                                                                     
+export HISTFILE="$PWD/.dir_bash_history"
+#timestamp all history entries                                                                            
+export HISTTIMEFORMAT="%h/%d - %H:%M:%S "
+export HISTCONTROL=ignoredups:erasedups
+export HISTSIZE=1000000
+export HISTFILESIZE=1000000
+shopt -s histappend ## append, no clearouts                                                               
+shopt -s histverify ## edit a recalled history line before executing                                      
+shopt -s histreedit ## reedit a history substitution line if it failed                                    
+
+## Save the history after each command finishes                                                           
+## (and keep any existing PROMPT_COMMAND settings)                                                        
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
