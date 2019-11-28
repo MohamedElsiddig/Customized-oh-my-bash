ef722b0e23ff72deac1a031dd8a3b1a95319b4c2
MohamedElsiddig <mxlr7siddig@gmail.com>
2019-11-28 21:16:50 +0200

    Added netspeed function

--- plugins/connection.plugin.sh
+++ plugins/connection.plugin.sh
@@ -1,6 +1,141 @@
 #!/bin/bash
 cite about-plugin
 about-plugin 'This plugin helps you to check your internet connection status'
+#################################################
+#Check netspeed function got it from https://github.com/timjaanson/netspeed/blob/master/netspeed.sh
+#################################################
+
+netspeed(){
+calcDelay=85 #in ms
+unit=1
+unitMarking="B"
+interface=":"
+formatValue=""
+interval=1
+count=1
+infinite=0
+
+
+#while getopts 'bBf:I:i:c:h' OPTION; do
+#  case "$OPTION" in
+#    b)
+#      unit=8
+#      unitMarking="b"
+#      ;;
+#    B)
+#      unit=1
+#      unitMarking="B"
+#      ;;
+
+#    f)
+#      formatValue="$OPTARG"
+#      ;;
+
+#    I)
+#      interface="$OPTARG"
+#      ;;
+
+#    i)
+#      interval="$OPTARG"
+#      
+#      ;;
+
+#    c)
+#      count="$OPTARG"
+#      if (( count == 0));then
+#          infinite=1
+#      fi
+#      ;;
+#    ?)
+#      #echo "script usage: $(basename $0) [-I <interface>]" >&2
+#			:
+#      ;;
+#  esac
+#done
+shift "$(($OPTIND -1))"
+
+fullCalcDelay=$((interval * 1000 + calcDelay))
+getCurrentBytes () {
+    N=0
+    for i in $(awk '/:/ { print($1, $2, $10) }' < /proc/net/dev | grep "${1}")
+    do
+      initBytes[$N]="$i"
+      let "N= $N + 1"
+    done
+}
+
+printValue () {
+    getCurrentBytes $3
+    initRX=0
+    initTX=0
+    N=0
+    for i in "${initBytes[@]}"
+    do
+        if [[ $(( N % 3 )) == 1 ]]
+        then
+	          initRX=$((${initRX} + ${i}))
+        elif [[ $(( N % 3 )) == 2 ]]
+        then
+	          initTX=$((${initTX} + ${i}))
+        fi
+        let "N= $N + 1"
+    done
+
+    sleep ${4}
+
+    finalRX=0
+    finalTX=0
+    getCurrentBytes $3
+    N=0
+    for i in "${initBytes[@]}"
+    do
+        if [[ $(( N % 3 )) == 1 ]]
+        then
+	          finalRX=$((${finalRX} + ${i}))
+        elif [[ $(( N % 3 )) == 2 ]]
+        then
+	          finalTX=$((${finalTX} + ${i}))
+        fi
+        let "N= $N + 1"
+    done
+
+    downloadSpeed=$(( ($finalRX - $initRX) * ${1}))
+    downloadSpeed=`echo ${downloadSpeed} ${2} ${4} 1000 ${5} | awk '{printf "%.2f \n", $1/$2/$5*($3*$4)}'`
+    uploadSpeed=$(( ($finalTX - $initTX) * ${1}))
+    uploadSpeed=`echo ${uploadSpeed} ${2} ${4} 1000 ${5} | awk '{printf "%.2f \n", $1/$2/$5*($3*$4)}'`
+    echo "${downloadSpeed}   ${uploadSpeed}"
+}
+
+if [[ $formatValue == "k" || $formatValue == "K" ]]
+then
+    format=1000
+    formatValue="k"
+elif [[ $formatValue == "M" || $formatValue == "m" ]]
+then
+    format=1000000
+    formatValue="M"
+elif [[ $formatValue == "G" || $formatValue == "g" ]]
+then
+    format=10000000000
+    formatValue="G"
+else
+    format=1000
+    formatValue="k"
+fi
+
+
+echo	"download upload [${formatValue}${unitMarking}/s]"
+h=0
+while (( h < count || infinite == 1 ))
+do
+    printValue $unit $format $interface $interval $fullCalcDelay
+    ((h+=1))
+done
+
+return 0
+}
+
+
 ##################################################
 #Check internet connection function
 ##################################################
@@ -92,6 +227,10 @@ function chick_net()
         echo -e ${echo_bold_red} [ X ]${echo_bold_white}::${echo_bold_white}[Couldn\'t get Country Information]:${echo_bold_red} Due Bad Connection! ${echo_normal};
             echo ""
         fi
+        download_rate=$(netspeed | awk '{print $1}' | cut -f1 -d'd' | sed '/^$/d')
+        upload_rate=$(netspeed | awk '{print $2}' | cut -f1 -d'u' | sed '/^$/d')
+        echo -e "${echo_bold_cyan} [ * ]${echo_bold_white}::${echo_bold_white}[Your internet speed is]:"${echo_bold_blue} Donwload:${echo_yellow}${download_rate} ${echo_normal}[kB/s], ${echo_bold_blue}Upload:${echo_yellow}${upload_rate} ${echo_normal}[kB/s] ${echo_normal};
+            echo ""
         sleep 1
 
     fi
