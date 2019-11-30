f50a7d28219e8f1ed651d7c090d6a79b3433b8a5
MohamedElsiddig <mxlr7siddig@gmail.com>
2019-11-30 20:59:54 +0200

    Updated plugins/git.plugin.sh

--- plugins/git.plugin.sh
+++ plugins/git.plugin.sh
@@ -551,3 +551,68 @@ fi
 git checkout "$selected_branch"
 return $?
 }
+
+
+
+function git-ls(){
+  about 'List File status in the current git'
+  group 'git'
+  example '$ git-ls'
+# Colors
+green='\e[0;32m%-3s\e[0m'
+red='\e[0;31m%-3s\e[0m'
+blue='\e[0;34m%-3s\e[0m'
+purple='\e[0;35m%-3s\e[0m'
+nc='\e[0m%-3s' # no color
+
+# Make sure this is a git repo
+git status --porcelain &> /dev/null
+if [ $? -ne 0 ]
+then
+	echo "Not a Git repository!"
+	return 
+fi
+
+if [[ `which exa` ]]
+	then
+		lsout=`exa -lh --git`
+	else
+		lsout=`ls -lh`
+	fi
+IFS=$'\n' lslines=($lsout)
+
+# ls -ls every file, shimming a git status for the working tree in front
+i=1
+for file in *
+do
+	gitstatus=`git status --porcelain $file`
+	if [ -z "$gitstatus" ]
+	then
+		stat="-"
+	else
+		stat=${gitstatus:1:1} # grab the second char, which is the working tree status
+	fi
+
+	case "$stat" in
+		"?")
+			printf $nc $stat
+			;;
+		"M")
+			printf $red $stat
+			;;
+		"A")
+			printf $blue $stat
+			;;
+		"D")
+			printf $purple $stat
+			;;
+		"-")
+			printf $green "OK"
+			;;
+	esac
+
+	echo ${lslines[i]}
+	((i++))
+
+done
+}
