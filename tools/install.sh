fc0f6a0ab928a58e3622c9750bc01c4cad322c60
MohamedElsiddig <mxlr7siddig@gmail.com>
2019-12-02 15:15:37 +0200

    Updated tools/install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -89,32 +89,7 @@ export OSH=$OSH
 
 	echo -e "\033[0;32mEnabling reasonable defaults\033[0m"
   # Load dependencies for enabling components
-	
-  source "$OSH/lib/composure.sh"
-	source "$OSH/lib/utilities.sh"
-	cite _about _param _example _group _author _version
-	source "$OSH/lib/helpers.sh"
-
-  to_be_load_plugins=(base alias-completion)
-
-  to_be_load_aliases=(general ls)
-  
-  to_be_load_completions=(base hints oh-my-bash reference)
-
-  for load_plugins in ${to_be_load_plugins[@]}
-    do
-      oh-my-bash enable plugin $load_plugins
-  done
-
-  for load_aliases in ${to_be_load_aliases[@]}
-    do
-      oh-my-bash enable alias $load_aliases  
-  done
-
-  for load_completions in ${to_be_load_completions[@]}
-    do
-      oh-my-bash enable completion $load_completions
-  done
+	env BASH=$OSH sh $OSH/tools/to_enable.sh
   
   # MOTD message :)
   printf '%s' "$GREEN"
