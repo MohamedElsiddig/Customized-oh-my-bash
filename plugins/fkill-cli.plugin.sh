9732d316a11ce7c521477d9daf8ae5c62594940f
MohamedElsiddig <mxlr7siddig@gmail.com>
2019-12-07 13:49:49 +0200

    Updated plugins/fkill-cli.plugin.sh

--- plugins/fkill-cli.plugin.sh
+++ plugins/fkill-cli.plugin.sh
@@ -15,13 +15,18 @@ elif [[ `which fzy` ]]
 fi
 if [[ ! -z $fuzzy_command ]]
 	then
-		process=`ps -A | awk '{print $1 "\t" $4}' | $fuzzy_command --prompt=Running\ processes:  | awk '{print $1}'`
+		process=`ps -A | awk '{print $1 "\t" $4}' | $fuzzy_command --reverse --border --prompt=Running\ processes:  | awk '{print $1}'`
 		if [[ ! -z $process ]] 
 			then
 				process_name=`ps -p $process -o comm=`
-				kill -9 $process
-				echo ""
-				echo -e ${echo_bold_green} ✔${echo_bold_yellow} Process $process_name [ id = $process ] were killed successfully${echo_normal}
+				kill -9 $process > /dev/null 2>&1
+				if [[ $? == 0 ]]
+					then
+						echo ""
+						echo -e ${echo_bold_green} ✔${echo_bold_yellow} Process $process_name [ id = $process ] were killed successfully${echo_normal}
+					else
+						echo -e ${echo_bold_red} ✘${echo_bold_yellow} Failed to kill Process $process_name [ id = $process ] ${echo_normal}
+				fi
 			else
 				:
 		fi
