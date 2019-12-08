9770e70c6c8a09824a3bc2661b991604ca8ab035
MohamedElsiddig <mxlr7siddig@gmail.com>
2019-12-08 13:57:08 +0200

    Updated plugins/sudo.plugin.sh

--- plugins/sudo.plugin.sh
+++ plugins/sudo.plugin.sh
@@ -3,7 +3,7 @@ about-plugin 'Sudo will be inserted before the command when pressing alt + s'
 sudo_command() {
 	if [[ -z $READLINE_LINE ]] 
 	     then
-	      READLINE_LINE="sudo $(sed -n '$p' $HOME/.bash_history)"
+	      READLINE_LINE="sudo $(sed -n '$p' $HISTFILE)"
 	elif [[ $READLINE_LINE == sudo\ * ]]; then 
              READLINE_LINE="${READLINE_LINE#sudo }"
 	elif [[ -n $READLINE_LINE ]];then  
