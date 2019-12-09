28d4af49ddcf8372d5489322df7b1594836dd1cf
MohamedElsiddig <mxlr7siddig@gmail.com>
2019-12-09 17:14:37 +0200

    develop: command-not-found missing after applying some plugins

---
+++ lib/command-not-found.sh
@@ -0,0 +1,26 @@
+
+function_exists () {
+    # Zsh returns 0 even on non existing functions with -F so use -f
+    declare -f $1 > /dev/null
+    return $?
+}
+
+if function_exists command_not_found_handle; then
+    if ! function_exists orig_command_not_found_handle; then
+        eval "orig_$(declare -f command_not_found_handle)"
+    fi
+else
+    orig_command_not_found_handle () {
+		if [[ -x /usr/lib/command-not-found ]]	
+			then
+				/usr/lib/command-not-found $1
+    	    	return 127
+			else
+				return 127
+		fi
+    }
+fi
+
+command_not_found_handle () {
+    orig_command_not_found_handle "$@"
+}
