992f3013f6a70c761cf22b4ff96704bd1550f082
MohamedElsiddig <mxlr7siddig@gmail.com>
2019-12-20 18:25:58 +0200

    Updated plugins/repeat-history.plugin.sh

--- plugins/repeat-history.plugin.sh
+++ plugins/repeat-history.plugin.sh
@@ -2,7 +2,7 @@
 cite about-plugin
 about-plugin 'A small but useful command for a linux shell Using the fuzzy finder It makes the bash history more easily accessible using "rh" function'
 function rh(){
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:-1,bg:-1,hl:#517599 --color=fg+:#cc6262,bg+:#262626,hl+:#5fd7ff --color=info:#afaf87,prompt:#ff1279,pointer:#af5fff --color=marker:#87ff00,spinner:#af5fff,header:#87afaf'
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:-1,bg:-1,hl:#517599 --color=fg+:#cc6262,bg+:#262626,hl+:#5fd7ff --color=info:4,prompt:#ff1279,pointer:#af5fff --color=marker:#87ff00,spinner:#af5fff,header:#87afaf'
 
     if [[ -z $_FILTER ]]; then
         _FILTER="fzf:fzy:fzf-tmux:peco:percol:gof:pick:icepick:sentaku:selecta"
@@ -43,7 +43,7 @@ then
     #read -p "Enter the number of desired command: " input
 	if [[ _FILTER="fzf"  ]]
 		then
-			command=$(cat ~/.repeat-history |fzf --exact --border --reverse --cycle  --height=45% --query "$READLINE_LINE" --prompt='Search History: ')
+			command=$(cat ~/.repeat-history |fzf --inline-info --exact --border --reverse --cycle  --height=45% --query "$READLINE_LINE" --prompt='Search History: ')
 	elif [[ _FILTER="fzy"  ]]
 		then
 			command=$(cat ~/.repeat-history |fzy --query "$READLINE_LINE" --prompt='Search History: ')
