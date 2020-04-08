82b7abe8a92d3cb5e0c5a467daf4a848e7b116d6
MohamedElsiddig <mxlr7siddig@gmail.com>
2020-04-08 18:57:38 +0200

    exports.sh stores user environment variables

---
+++ lib/exports.sh
@@ -0,0 +1,1 @@
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:-1,bg:-1,hl:#517599 --color=fg+:#cc6262,bg+:#262626,hl+:#5fd7ff --color=info:4,prompt:#ff1279,pointer:#af5fff --color=marker:#87ff00,spinner:#af5fff,header:#87afaf'
