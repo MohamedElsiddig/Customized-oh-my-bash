50a8d610b6a6558e1f64cb27db01c85e2695f464
MohamedElsiddig <mxlr7siddig@gmail.com>
2020-05-23 21:07:53 +0200

    Updated lib/command-not-found.sh

--- lib/command-not-found.sh
+++ lib/command-not-found.sh
@@ -1,27 +1,38 @@
 #!/usr/bin/env bash
-#Because sometimes the "/usr/lib/command-not-found" doesn't work after applying some plugins
-function_exists () {
-    # Zsh returns 0 even on non existing functions with -F so use -f
-    declare -f $1 > /dev/null
-    return $?
-}
-
-if function_exists command_not_found_handle; then
-    if ! function_exists orig_command_not_found_handle; then
-        eval "orig_$(declare -f command_not_found_handle)"
-    fi
-else
-    orig_command_not_found_handle () {
-		if [[ -x /usr/lib/command-not-found ]]	
-			then
-				/usr/lib/command-not-found -- $1
-    	    	return $? 
-			else
-				return 127
-		fi
-    }
+if [[ -r /etc/os-release ]]; then
+    distro=$(awk -F'=' '"NAME" == $1 { gsub("\"", "", $2); print tolower($2); }' /etc/os-release)
+    distro="${distro%% *}"
 fi
 
-command_not_found_handle () {
-    orig_command_not_found_handle "$@"
-}
+case "$distro" in
+    debian|ubuntu) 
+        #Because sometimes the "/usr/lib/command-not-found" doesn't work after applying some plugins
+        function_exists () {
+            # Zsh returns 0 even on non existing functions with -F so use -f
+            declare -f $1 > /dev/null
+            return $?
+        }
+
+        if function_exists command_not_found_handle; then
+            if ! function_exists orig_command_not_found_handle; then
+                eval "orig_$(declare -f command_not_found_handle)"
+            fi
+        else
+            orig_command_not_found_handle () {
+                if [[ -x /usr/lib/command-not-found ]]	
+                    then
+                        /usr/lib/command-not-found -- $1
+                        return $? 
+                    else
+                        return 127
+                fi
+            }
+        fi
+
+        command_not_found_handle () {
+            orig_command_not_found_handle "$@"
+        }
+        ;;
+
+    *) : ;;
+esac
