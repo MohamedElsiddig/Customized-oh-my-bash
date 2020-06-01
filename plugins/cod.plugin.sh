04f7ac00953d648a3feb49bf97a8209803551833
MohamedElsiddig <mxlr7siddig@gmail.com>
2020-06-01 21:18:40 +0200

    Cod is a completion daemon for {bash,zsh}

---
+++ plugins/cod.plugin.sh
@@ -0,0 +1,21 @@
+
+cite about-plugin
+about-plugin 'Cod is a completion daemon It detects usage of --help commands parses their output and generates auto-completions for your shell.'
+
+if [[ `command -v  cod`]]
+	then
+        source <(cod init $$ bash)
+	else
+        echo ""
+ 		source "${OSH}/themes/colours.theme.sh"
+		source "${OSH}/themes/base.theme.sh"
+ 		sleep 1
+		echo -e "${echo_bold_yellow} [WARRNING]${echo_normal} cod is not found in your path please install and reEnable this plugin${echo_normal}" 
+		echo ""
+		sleep 1
+		echo -e "${echo_bold_cyan} [INFO]${echo_normal} Disabling it to make sure it doesn't effect the bash startup time
+		${echo_normal}"
+		sleep 1
+		_disable-plugin cod 
+fi
+fi
